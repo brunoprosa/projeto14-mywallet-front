96b14deba490b8885be00461644fe1eda2526c9a
brunoprosa <bruno.prosa01@gmail.com>
2023-08-27 15:39:45 -0300

    cadastro e login concluido

--- src/pages/SignInPage.jsx
+++ src/pages/SignInPage.jsx
@@ -1,18 +1,58 @@
 import styled from "styled-components"
-import { Link } from "react-router-dom"
+import { useNavigate, Link } from "react-router-dom"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useState } from "react"
+import axios from "axios"
+import dotenv from "dotenv"
 
 export default function SignInPage() {
+
+  const [email, setEmail] = useState("")
+	const [senha, setSenha] = useState("")
+
+  dotenv.config()
+  const navigate = useNavigate()
+
+  function singIn (e) {
+
+    e.preventDefault()
+
+		const requisicao = axios.post(`${import.meta.env.VITE_API_URL}/`, {
+			email,
+      senha
+		})
+
+    requisicao.then(resp => {
+        localStorage.setItem("token", resp.data.token);
+        navigate("/home")
+      })
+    requisicao.catch(err => alert(err))
+
+  }
+
   return (
     <SingInContainer>
       <form>
-        <MyWalletLogo />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
+        <MyWalletLogo onSubmit={singIn} />
+        <input 
+          placeholder="E-mail" 
+          type="email" 
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          required 
+        />
+        <input 
+          placeholder="Senha" 
+          type="password" 
+          autocomplete="new-password"
+          value={senha}
+          onChange={e => setSenha(e.target.value)}
+          required  
+        />
         <button>Entrar</button>
       </form>
 
-      <Link>
+      <Link to = '/cadastro'>
         Primeira vez? Cadastre-se!
       </Link>
     </SingInContainer>
--- src/pages/SignUpPage.jsx
+++ src/pages/SignUpPage.jsx
@@ -1,20 +1,75 @@
-import { Link } from "react-router-dom"
+import { Link, useNavigate } from "react-router-dom"
 import styled from "styled-components"
 import MyWalletLogo from "../components/MyWalletLogo"
+import { useState } from "react"
+import axios from "axios"
+import dotenv from "dotenv"
 
 export default function SignUpPage() {
+
+  const [email, setEmail] = useState("")
+	const [senha, setSenha] = useState("")
+  const [senha2, setSenha2] = useState("")
+  const [nome, setNome] = useState("")
+
+  dotenv.config()
+  const navigate = useNavigate()
+
+  function singUp (e) {
+
+    e.preventDefault()
+
+    if(senha !== senha2) return alert('senhas se diferem')
+
+		const requisicao = axios.post(`${import.meta.env.VITE_API_URL}/cadastro`, {
+			nome, 
+      email,
+      senha
+		})
+
+    requisicao.then(() => navigate("/"))
+    requisicao.catch((err) => alert(err))
+
+  }
+
   return (
     <SingUpContainer>
-      <form>
+      <form onSubmit={singUp}>
         <MyWalletLogo />
-        <input placeholder="Nome" type="text" />
-        <input placeholder="E-mail" type="email" />
-        <input placeholder="Senha" type="password" autocomplete="new-password" />
-        <input placeholder="Confirme a senha" type="password" autocomplete="new-password" />
+        <input 
+          placeholder="Nome" 
+          type="text" 
+          value={nome}
+          onChange={e => setNome(e.target.value)}
+          required 
+        />
+        <input 
+          placeholder="E-mail" 
+          type="email" 
+          value={email}
+          onChange={e => setEmail(e.target.value)}
+          required 
+        />
+        <input 
+          placeholder="Senha" 
+          type="password" 
+          autocomplete="new-password"
+          value={senha}
+          onChange={e => setSenha(e.target.value)} 
+          required 
+        />
+        <input 
+          placeholder="Confirme a senha" 
+          type="password" 
+          autocomplete="new-password" 
+          value={senha2}
+          onChange={e => setSenha2(e.target.value)}
+          required 
+        />
         <button>Cadastrar</button>
       </form>
 
-      <Link>
+      <Link to = '/'>
         Já tem uma conta? Entre agora!
       </Link>
     </SingUpContainer>
